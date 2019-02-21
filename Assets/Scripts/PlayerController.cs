2c9734e8e05e7dde375c3b1cd78ec0f519fd5067
Can Delibas <candelibas96@gmail.com>
2019-02-21 23:17:28 +0200

    prototype

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float movementSpeed = 5f;
+    public float twirlSpeed = 180f;
+    
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        transform.Rotate(0, 0, 180 * Time.deltaTime);
+        transform.Rotate(0, 0, twirlSpeed * Time.deltaTime);
+
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        moveVelocity = moveInput.normalized * movementSpeed;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.gameObject.name);
     }
 }
