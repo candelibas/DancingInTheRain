6b7e7edc99b03e3f19f4efdb6fbdbbb7d32d1e67
Can Delibas <candelibas96@gmail.com>
2019-02-22 16:18:28 +0200

    add game scene and camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float step = 0.01f;
+
+    [SerializeField]
+    private Transform wayPoint;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var cameraPosition = Camera.main.gameObject.transform.position;
+        
+        if(cameraPosition.y >= wayPoint.position.y)
+        {
+            // stop camera
+            cameraPosition.y = wayPoint.position.y;
+        }
+        else
+        {
+            cameraPosition.y += step;
+            Camera.main.gameObject.transform.position = cameraPosition;
+        }
+    }
+}
