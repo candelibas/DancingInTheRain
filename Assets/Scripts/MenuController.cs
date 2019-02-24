255b703764ef62990f4e06337617bcacd96a1014
Can Delibas <candelibas96@gmail.com>
2019-02-24 04:37:25 +0200

    add main menu scene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public int index;
+    public string sceneName;
+
+    public Image black;
+    public Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        StartCoroutine(Fading());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene(index);
+    }
+}
