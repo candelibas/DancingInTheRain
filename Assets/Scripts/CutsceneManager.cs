70db0200ad4768ba97ffc4efffabbc3c1a2b46d6
Can Delibas <candelibas96@gmail.com>
2019-02-24 22:25:43 +0200

    add beginningcutscene

---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CutsceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image nextCutScene;
+    
+    void Start()
+    {
+        
+    }
+
+    public void AnimationComplete()
+    {
+        if(nextCutScene != null)
+        {
+            nextCutScene.gameObject.SetActive(true);
+        }
+        else
+        {
+            // Go to gameplay scene
+            SceneManager.LoadScene(1);
+        }
+
+    }
+
+
+}
